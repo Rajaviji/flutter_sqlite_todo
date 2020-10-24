a1990a8622c3c520da24e20321eb3303bc7ea87f
Rajarajan <mac@macs-MacBook-Air.local>
2020-10-24 17:06:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/todo.dart';
+
+import 'DatabaseHelper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Listify'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textController = new TextEditingController();
+  List<Todo> taskList = new List();
+  @override
+  void initState() {
+    super.initState();
+    DatabaseHelper.instance.queryAllRows().then((value) {
+      setState(() {
+        value.forEach((element) {
+          taskList.add(Todo(id: element['id'], title: element["title"]));
+        });
+      });
+    }).catchError((error) {
+      print(error);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+        alignment: Alignment.topLeft,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: "Enter a task"),
+                    controller: textController,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addToDb,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                  child: taskList.isEmpty
+                      ? Container()
+                      : ListView.builder(
+                          itemBuilder: (ctx, index) {
+                            if (index == taskList.length) return null;
+                            return ListTile(
+                              title: Text(taskList[index].title),
+                              leading: Text(taskList[index].id.toString()),
+                              trailing: IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () =>
+                                    _deleteTask(taskList[index].id),
+                              ),
+                            );
+                          },
+                        )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _deleteTask(int id) async {
+    await DatabaseHelper.instance.delete(id);
+    setState(() {
+      taskList.removeWhere((element) => element.id == id);
+    });
+  }
+
+  void _addToDb() async {
+    String task = textController.text;
+    var id = await DatabaseHelper.instance.insert(Todo(title: task));
+    setState(() {
+      taskList.insert(0, Todo(id: id, title: task));
+    });
+  }
+}
